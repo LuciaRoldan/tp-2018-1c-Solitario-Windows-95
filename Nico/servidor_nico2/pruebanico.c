6afc13956baf7c679ee1e9a52d3232b4eb0db32c
ndeberuti <saxo_nico@hotmail.com>
2018-04-19 20:51:27 -0300

    servidor basico con un log vacio

---
+++ Nico/servidor_nico2/pruebanico.c
@@ -0,0 +1,174 @@
+#define BACKLOG 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <semaphore.h>
+#include <commons/collections/list.h>
+#include <commons/string.h>
+#include <commons/log.h>
+#include <commons/bitarray.h>
+#include <commons/config.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <math.h>
+
+
+
+void sigchld_handler(int s)
+	{
+	    // waitpid() might overwrite errno, so we save and restore it:
+	    int saved_errno = errno;
+
+	    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+	    errno = saved_errno;
+	}
+
+
+	// get sockaddr, IPv4 or IPv6:
+	void *get_in_addr(struct sockaddr *sa)
+	{
+	    if (sa->sa_family == AF_INET) {
+	        return &(((struct sockaddr_in*)sa)->sin_addr);
+	    }
+
+	    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+char* ip;
+int puerto;
+
+
+	t_config* configuracion=config_create("/home/utnso/tp-2018-1c-Solitario-Windows-95/Nico/configuracion_prueba");
+
+	ip=config_get_string_value(configuracion,"IP");
+	puerto=config_get_int_value(configuracion,"PUERTO");
+
+//servidor(ip,puerto);
+
+	printf("%s : %d\n", ip, puerto);
+
+
+
+
+	    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+		    struct addrinfo hints, *servinfo, *p;
+		    struct sockaddr_storage their_addr; // connector's address information
+		    socklen_t sin_size;
+		    struct sigaction sa;
+		    int yes=1;
+		    char s[INET6_ADDRSTRLEN];
+		    int rv;
+
+		    memset(&hints, 0, sizeof hints);
+		    hints.ai_family = AF_UNSPEC;
+		    hints.ai_socktype = SOCK_STREAM;
+		    hints.ai_flags = AI_PASSIVE; // use my IP
+
+		    if ((rv = getaddrinfo(NULL, puerto, &hints, &servinfo)) != 0) {
+		        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		        return 1;
+		    }
+
+		    // loop through all the results and bind to the first we can
+		    for(p = servinfo; p != NULL; p = p->ai_next) {
+		        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+		                p->ai_protocol)) == -1) {
+		            perror("server: socket");
+		            continue;
+		        }
+
+		        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		                sizeof(int)) == -1) {
+		            perror("setsockopt");
+		            exit(1);
+		        }
+
+		        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+		            close(sockfd);
+		            perror("server: bind");
+		            continue;
+		        }
+
+		        break;
+		    }
+
+		    freeaddrinfo(servinfo); // all done with this structure
+
+		    if (p == NULL)  {
+		        fprintf(stderr, "server: failed to bind\n");
+		        exit(1);
+		    }
+
+		    if (listen(sockfd, BACKLOG) == -1) {
+		        perror("listen");
+		        exit(1);
+		    }
+
+		    sa.sa_handler = sigchld_handler; // reap all dead processes
+		    sigemptyset(&sa.sa_mask);
+		    sa.sa_flags = SA_RESTART;
+		    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		        perror("sigaction");
+		        exit(1);
+		    }
+
+		    printf("server: waiting for connections...\n");
+
+		    while(1) {  // main accept() loop
+		        sin_size = sizeof their_addr;
+		        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		        if (new_fd == -1) {
+		            perror("accept");
+		            continue;
+		        }
+
+		        inet_ntop(their_addr.ss_family,
+		            get_in_addr((struct sockaddr *)&their_addr),
+		            s, sizeof s);
+		        printf("server: got connection from %s\n", s);
+
+		        if (!fork()) { // this is the child process
+		            close(sockfd); // child doesn't need the listener
+		            if (send(new_fd, "Hello, world!", 13, 0) == -1)
+		                perror("send");
+		            close(new_fd);
+		            exit(0);
+		        }
+		        close(new_fd);  // parent doesn't need this
+		    }
+
+
+
+
+
+	return 0;
+}
+
+
