c74f558929156436bdff1f1fdbcd5a2e11b7f1dd
Ro <rociochipian@gmail.com>
2018-04-19 00:31:13 -0300

    No lo ordene nada perdon

---
+++ Instancia/instancia.c
@@ -0,0 +1,21 @@
+char* ipCoordinador;
+int puertoCoordinador;
+algoritmoReemplazo; CIRC LRU o BSU;
+puntoDeMontaje; //path absoluto ???
+char* nombreInstanc--+-1kia;
+int intervaloDump;
+
+
+
+void inicializar_instancia{
+	recibir_configuracion_inicial;
+}
+
+
+
+void send_content;
+void contestar_solicitud;
+
+
+void recibir_configuracion_inicial;
+void inicializar_instancia;
