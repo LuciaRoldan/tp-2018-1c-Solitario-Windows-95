5a381b79fbc76597f5ce3c88c1ff4b504e892c61
Your Name <imazzini@est.frba.utn.edu.ar>
2018-04-25 21:40:54 -0300

    Se agrega funciones en seudo de la instancia

--- Instancia/instancia.c
+++ Instancia/instancia.c
@@ -1,3 +1,5 @@
+#include "commons_propias.h"
+
 char* ipCoordinador;
 int puertoCoordinador;
 //algoritmoReemplazo; CIRC LRU o BSU;
@@ -5,17 +7,51 @@ int puertoCoordinador;
 char* nombreInstancia;
 int intervaloDump;
 
+int main() {
 
+	int puertoDeLlegada, PUERTO_ESI, PUERTO_COORDINADOR;
 
-//void inicializar_instancia{
-//	recibir_configuracion_inicial;
-//}
+	int socket = connect_to_server("127.0.0.1", 8000); //preguntar como hacer que lo traiga de nuestras commons
+/*
+	struct mensaje mensaje = recv(socket, buffer, len_mensaje, 0); //recibe el mensaje del coordinador
 
+	void procesar_mensaje(mensaje) {
 
+		while (1) {
 
-void send_content;
-void contestar_solicitud;
+			instruccion = mensaje->intruccion; //del mensaje obtenemos la instruccion
+			clave = mensaje->clave;
+			int respuesta;
+			acceder_a_entrada(clave); //ingresa a la tabla de entradas segun la clave que se manda por el mensaje
+			switch (instruccion) { // la instruccion puede ser GET,SET o STORE
+			case (GET): //con el get la instancia bloquea la clave de la entrada
+				bloquearClave();
+				return 1; //si todo sale bien retorna un 1 sino 0( no se como verifica que salga bien)
+				break;
+			case (SET): //guarda la info
+				almacenar_infor(); //
+				return 1; //exito
+				break;
+			case (STORE): //guarda la info y desbloquea clave de la entrada
+				almacenar_info();
+				desbloquearClave();
+				return 1;
+				break;
+			}
+			enviar_resultado_coordinador ();
+		}
+*/
+	}
 
+	void send_content;
+	void contestar_solicitud;
 
-void recibir_configuracion_inicial;
-void inicializar_instancia;
+	void recibir_configuracion_inicial;
+	void inicializar_instancia;
+
+	void bloquear_clave();
+
+	void procesar_mensaje( mensaje);
+	void enviar_resultado_coordinador();
+}
+
