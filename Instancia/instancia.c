22548694832e288364104bf87457ef48b147f235
Your Name <imazzini@est.frba.utn.edu.ar>
2018-04-27 18:35:12 -0300

    algo de instancia

--- Instancia/instancia.c
+++ Instancia/instancia.c
@@ -7,8 +7,13 @@ int puertoCoordinador;
 char* nombreInstancia;
 int intervaloDump;
 
-int main() {
+typedef struct {
+	char instruccion[];
+	int clave;
+}__attribute__((packed)) Mensaje_Instancia;
+//arma otro tipo de struct (local) que separe la instruccion de la clave
 
+int main() {
 
 //seria como inicializar la instancia
 	int socket = connect_to_server("127.0.0.1", "8081");
@@ -19,24 +24,37 @@ int main() {
 //El recv guarda la cantidad de bytes no el mensaje por lo tanto este se va a guardar en el "buffer" mensaje que creamos
 //probar que la cant de bytes sea mayor a cero
 
-	void procesar_mensaje(Mensaje mensaje) {
+	void procesar_mensaje(Mensaje_Instancia mensaje) {
 
 		while (1) {
 
 			char * instruccion = strcpy(mensaje->instruccion, instruccion); //del mensaje obtenemos la instruccion
-			//como sacamos la clavee de la instruccion???? vamos a tener que armar otro tipo de struct que separe la instruccion de la clave
-			/*
-			 acceder_a_entrada(clave); //ingresa a la tabla de entradas segun la clave que se manda por el mensaje
-			 int respuesta;
-			 //no vamos a necesitar un switch porque la instancia solo lee o guarda es decir GET y SET
+			char * clave = strcpy(mensaje->clave, clave); //obtenemos la clave del mensaje
 
-			 if (instruccion == "GET") {
-			 respuesta = read_archivo();
-			 } else {
-			 respuesta = guardar_archivo();
-			 }
-			 send_mensaje(socket, respuesta);
-			 //aca supuestamente le avisa al coordinador como salio pero no se si la respuesta va como el numero directo o habria que armarla en un protocolo
-			 */
+			acceder_entrada(clave); //ingresa a la tabla de entradas segun la clave que se manda por el mensaje
+			int respuesta;
+			//no vamos a necesitar un switch porque la instancia solo lee o guarda es decir GET y SET
+
+			if (instruccion == "GET") {
+				respuesta = read_archivo();
+			} else {
+				respuesta = guardar_archivo();
+			}
+			send_mensaje(socket, respuesta);
+			//aca supuestamente le avisa al coordinador como salio pero no se si la respuesta va como el numero directo o habria que armarla en un protocolo
+
 		}
 	}
+
+	void acceder_entrada(int clave) {
+
+	}
+
+	int read_archivo() {
+
+	}
+
+	guardar_archivo();
+
+}
+
