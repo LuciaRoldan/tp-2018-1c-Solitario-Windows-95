e9796e2b8664de32f7c0851a734b73a7fe3ccd28
LuciaRoldan <lucia_roldan@outlook.com>
2018-05-07 22:56:43 -0300

    Emprolijo las commons propias

--- Commons_propias/Commons_propias/commons_propias.c
+++ Commons_propias/Commons_propias/commons_propias.c
@@ -1,5 +1,160 @@
 #include "commons_propias.h"
 
+
+int connect_to_server(char * ip, char * port, t_log * logger){
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0 ,sizeof(hints));
+	hints.ai_family = AF_UNSPEC; //se fija solo si es IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM; //protocolo TCP
+
+	getaddrinfo(ip, port, &hints, &server_info); //guarda en server_info los datos de la conexion
+
+	int server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol); //creo el socket con server info
+
+	int valorConnect = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
+
+	freeaddrinfo(server_info); //libero el arreglo
+
+	if (valorConnect < 0) { //ver niveles de logs de commons, verifico por error
+		_exit_with_error(server_socket, "No me pude conectar al servidor", NULL);
+	}
+
+		log_info(logger, "Conectado!"); //logeo exito
+		return server_socket;
+}
+
+
+int inicializar_servidor(char* ip, char* puerto, t_log * logger){
+
+	//struct sockaddr_in direccionServidor;
+	     // fill in my IP for me
+
+	struct addrinfo hints, *res;
+
+	memset(&hints,0,sizeof hints);
+
+	hints.ai_family = AF_UNSPEC; //que se fije solon si es IPv4 o IPv6
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = 0;
+
+	getaddrinfo(ip, puerto, &hints, &res);
+	int servidor = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+	int activado = 1;
+		setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+		if(bind(servidor, (void*) &hints, sizeof(hints)) != 0){
+			_exit_with_error(servidor, "Fallo el bind", NULL);
+
+		}
+
+		printf("estoy escuchando\n");
+		log_info(logger, "Escuchando!");
+		listen(servidor, 100);
+
+	//--------------------------------------
+
+	struct sockaddr_in direccionCliente;
+	unsigned int tamanoDireccion;
+	int cliente = accept(servidor, (void*) &direccionCliente,&tamanoDireccion);
+	printf("recibi una conexion en %d\n", cliente);
+
+	return cliente;
+
+}
+
+
+int recv_string(int socket){
+
+	int bytes_recv;
+	char* mensaje_recibido [10];
+	bytes_recv= recv(socket,mensaje_recibido, 11,0);
+	printf("%s",&mensaje_recibido);
+	return bytes_recv;
+}
+
+
+void send_mensaje(int socket, Mensaje mensaje) {
+
+	log_info(logger, "Enviando mensaje");
+	mensaje.id_mensaje = 99;
+	int resultado = send(socket, &mensaje, sizeof(Mensaje) , 0);
+	if(resultado <= 0) {
+		_exit_with_error(socket, "No se pudo enviar el mensaje", NULL);
+	}
+}
+
+
+void *wait_content(int socket) {
+
+	log_info(logger, "Esperando el encabezado del contenido(%ld bytes)", sizeof(ContentHeader));
+
+	ContentHeader * header = (ContentHeader*) malloc(sizeof(ContentHeader));
+
+	if (recv(socket, header, sizeof(ContentHeader), 0) <= 0) {
+		_exit_with_error(socket, "No se pudo recibir el encabezado del contenido", header);
+	}
+
+	if (header->id != 18) {
+		_exit_with_error(socket, "Id incorrecto, deberia ser 18", header);
+	}
+
+	log_info(logger, "Esperando el contenido (%d bytes)", header->len);
+
+	void * buf = calloc(sizeof(char), header->len + 1);
+	if (recv(socket, buf, header->len, MSG_WAITALL) <= 0) {
+		free(buf);
+		_exit_with_error(socket, "Error recibiendo el contenido", header);
+	}
+
+	log_info(logger, "Contenido recibido '%s'", (char*) buf);
+	free(header);
+	return buf;
+}
+
+
+void send_content(int socket, void * content, int id) {
+
+	int longitud = sizeof(&content);
+	ContentHeader header = { .id = id, .len = longitud };
+	int result_header = send(socket, &header,sizeof(ContentHeader),0);
+
+	int message_size = sizeof(Mensaje) + longitud;
+	void * buf = malloc(message_size);
+
+	memcpy(buf, &content, sizeof(message_size));
+	free(content);
+
+	log_info(logger, "Enviando contenido");
+	int result_send =  send(socket, buf, message_size, 0);
+	free(buf);
+
+	if (result_send <= 0) {
+		_exit_with_error(socket, "No se pudo enviar el contenido", NULL);
+	}
+}
+
+
+void _exit_with_error(int socket, char* error_msg, void * buffer) {
+	if (buffer != NULL) {
+		free(buffer);
+	}
+	log_error(logger, error_msg);
+	close(socket);
+	exit_gracefully(1);
+}
+
+void exit_gracefully(int return_nr) {
+	log_destroy(logger);
+	exit(return_nr);
+}
+
+
+
+
+
 // COMMONS ESI //
 
 
@@ -27,85 +182,6 @@ typedef struct{
 
 // COMMONS CONEXIONES //
 
-
-
-
-int connect_to_server(char * ip, char * port, t_log * logger){
-	struct addrinfo hints;
-	struct addrinfo *server_info;
-
-	memset(&hints, 0 ,sizeof(hints));
-	hints.ai_family = AF_UNSPEC; //se fija solo si es IPv4 o IPv6
-	hints.ai_socktype = SOCK_STREAM; //protocolo TCP
-
-	getaddrinfo(ip, port, &hints, &server_info); //guarda en server_info los datos de la conexion
-
-
-
-	int server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol); //creo el socket con server info
-
-	int valorConnect = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
-
-	freeaddrinfo(server_info); //libero el arreglo
-
-	if (valorConnect < 0) { //ver niveles de logs de commons, verifico por error
-		_exit_with_error(server_socket, "No me pude conectar al servidor", NULL);
-	}
-
-		log_info(logger, "Conectado!"); //logeo exito
-		return server_socket;
-}
-
-
-int inicializar_servidor(char* ip, char* puerto, t_log * logger){
-
-
-	//struct sockaddr_in direccionServidor;
-	     // fill in my IP for me
-
-	struct addrinfo hints, *res;
-
-
-
-	memset(&hints,0,sizeof hints);
-
-
-	hints.ai_family = AF_UNSPEC; //que se fije solon si es IPv4 o IPv6
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = 0;
-
-
-	getaddrinfo(ip, puerto, &hints, &res);
-	int servidor = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-
-	int activado = 1;
-		setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
-
-		if(bind(servidor, (void*) &hints, sizeof(hints)) != 0){
-			_exit_with_error(servidor, "Fallo el bind", NULL);
-
-		}
-
-		printf("estoy escuchando\n");
-		log_info(logger, "Escuchando!");
-		listen(servidor, 100);
-
-	//--------------------------------------
-
-	struct sockaddr_in direccionCliente;
-	unsigned int tamanoDireccion;
-	int cliente = accept(servidor, (void*) &direccionCliente,&tamanoDireccion);
-	printf("recibi una conexion en %d\n", cliente);
-
-
-
-		return cliente;
-
-}
-
-
-
 /*
 int send_string(int socket, char* mensaje){
 int len, bytes_sent;
@@ -120,35 +196,7 @@ return bytes_sent;
 
 }
 */
-int recv_string(int socket){
 
-int bytes_recv;
-char* mensaje_recibido [10];
-
-bytes_recv= recv(socket,mensaje_recibido, 11,0);
-printf("%s",&mensaje_recibido);
-return bytes_recv;
-}
-
-
-
-
-
-
-void send_mensaje(int socket, Mensaje mensaje) {
-
-	log_info(logger, "Enviando mensaje");
-
-
-	mensaje.id_mensaje = 99;
-
-int resultado = send(socket, &mensaje, sizeof(Mensaje) , 0);
-if(resultado <= 0) {
-
-	_exit_with_error(socket, "No se pudo enviar el mensaje", NULL);
-}
-
-}
 /*
 void wait_mensaje(int socket){
 
@@ -167,80 +215,3 @@ void wait_mensaje(int socket){
 }
 */
 
-void *wait_content(int socket) {
-
-log_info(logger, "Esperando el encabezado del contenido(%ld bytes)", sizeof(ContentHeader));
-
-  ContentHeader * header = (ContentHeader*) malloc(sizeof(ContentHeader));
-
-  if (recv(socket, header, sizeof(ContentHeader), 0) <= 0) {
-    _exit_with_error(socket, "No se pudo recibir el encabezado del contenido", header);
-  }
-
-  if (header->id != 18) {
-    _exit_with_error(socket, "Id incorrecto, deberia ser 18", header);
-  }
-
-  log_info(logger, "Esperando el contenido (%d bytes)", header->len);
-
-
-  void * buf = calloc(sizeof(char), header->len + 1);
-  if (recv(socket, buf, header->len, MSG_WAITALL) <= 0) {
-    free(buf);
-    _exit_with_error(socket, "Error recibiendo el contenido", header);
-  }
-
-
-  log_info(logger, "Contenido recibido '%s'", (char*) buf);
-  free(header);
-  return buf;
-}
-
-
-void send_content(int socket, void * content, int id) {
-
-
-  int longitud = sizeof(&content);
-
-  ContentHeader header = { .id = id, .len = longitud };
-
-  int result_header = send(socket, &header,sizeof(ContentHeader),0);
-
-
-  int message_size = sizeof(Mensaje) + longitud;
-  void * buf = malloc(message_size);
-
-  memcpy(buf, &content, sizeof(message_size));
-
-
-
-
-  free(content);
-
-
-
-  log_info(logger, "Enviando contenido");
-  int result_send =  send(socket, buf, message_size, 0);
-  free(buf);
-
-  if (result_send <= 0) {
-    _exit_with_error(socket, "No se pudo enviar el contenido", NULL);
-  }
-}
-
-
-void _exit_with_error(int socket, char* error_msg, void * buffer) {
-  if (buffer != NULL) {
-    free(buffer);
-  }
-  log_error(logger, error_msg);
-  close(socket);
-  exit_gracefully(1);
-}
-
-void exit_gracefully(int return_nr) {
-
-  log_destroy(logger);
-  exit(return_nr);
-
-}
--- Commons_propias/Commons_propias/commons_propias.h
+++ Commons_propias/Commons_propias/commons_propias.h
@@ -1,10 +1,3 @@
-/*
- * commons_propias.h
- *
- *  Created on: 24 abr. 2018
- *      Author: utnso
- */
-
 #ifndef COMMONS_PROPIAS_H_
 #define COMMONS_PROPIAS_H_
 
@@ -30,7 +23,7 @@
 
 t_log * logger;
 
-//---------------------structs para mensajes
+///////////////////////// INSTRUCCIONES /////////////////////////
 
 typedef enum{ //posibles instrucciones
 	GET,
@@ -40,8 +33,7 @@ typedef enum{ //posibles instrucciones
 
 typedef char Clave [30]; //keys
 
-
-//-----------------------MENSAJES
+//////////////////////////// MENSAJES ////////////////////////////
 
 //ESI-COORDINADOR
 typedef struct{ //mensaje que manda el ESI al Coordinador
@@ -55,6 +47,7 @@ typedef enum{
 	BLOQUEARSE,
 	DESBLOQUEARSE
 } AccionESI;
+
 typedef struct{ //mensaje que manda Planificador a ESI
 	AccionESI instruccion;
 } InstruccionESI;
@@ -65,9 +58,7 @@ typedef struct{
 	int tamano_entrada;
 } datos_configuracion;
 
-
-
-//---------------------structs para conexions
+/////////////////////////// CONEXIONES ///////////////////////////
 
 //PARA EL PLANIFICADOR Y EL COORDINADOR
 typedef struct {
@@ -76,29 +67,25 @@ typedef struct {
 }t_conexion;
 
 
-
-
-
 typedef struct  {
   int id_mensaje;
   char instruccion [10];
 } __attribute__((packed)) Mensaje;
 //no se deberian usar punteros en las structs cuando van a ser llamadas por sockets
-// no nos va a alcanzar con un solo tipo de mensaje, tenemos que hacer varios dependiendo de quien lo reciba
+
 
 typedef struct {
   int id;
   int len;
 } __attribute__((packed)) ContentHeader;
 
-//------------------funciones
+//////////////////////////// FUNCIONES ////////////////////////////
 
 //funcoines de salida
 void _exit_with_error(int socket, char* error_msg, void * buffer);
 void exit_gracefully(int return_nr);
 
 //funciones de conexion
-
 int inicializar_servidor(char* ip, char* puerto, t_log* logger);//inicializa un servidor y espera conexiones
 int connect_to_server(char* ip, char * port, t_log* logger);//devuelve el socket con el que se establece la conexion a un servidor
 
@@ -106,16 +93,11 @@ int connect_to_server(char* ip, char * port, t_log* logger);//devuelve el socket
 void send_string(int socket, char* mensaje);
 void wait_string(int socket, int len);
 
-
 //funciones de recv() y send() variables para despues de serializacion
 void send_mensaje(int socket, Mensaje mensaje);//envia un struct tipo Mensaje
 void wait_mensaje(int socket);
 void * wait_content(int socket);//espera contenido de tamaño variable
 void send_content(int socket, void * content, int id);//envia contenido de tamaño variable
-
-
-
-
 
 
 #endif /* COMMONS_PROPIAS_H_ */
