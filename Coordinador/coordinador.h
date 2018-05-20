df3b1c86caf9a4b799973a75fbac484dda556f9a
LuciaRoldan <lucia_roldan@outlook.com>
2018-05-20 04:38:47 -0300

    Agrego funciones al metodo de handshake y de recibir y enviar al coordinador

--- Coordinador/coordinador.h
+++ Coordinador/coordinador.h
@@ -18,14 +18,26 @@ typedef struct{
 
 ///////////////////////// FUNCIONES /////////////////////////
 
+//INICIALIZACION
 info_archivo_config leer_archivo_configuracion(FILE* archivo);
 int inicializar_coordinador(info_archivo_config configuracion, t_log* logger);
 void conectar_planificador(int* socket_escucha, int* socket_planificador, t_log* logger);
+
+//COMUNICACION
 void enviar_configuracion_instancia(int* socket, info_archivo_config configuracion, t_log* logger);
 void enviar_pedido_esi(int esi_id, int* socket, t_esi_operacion instruccion, t_log* logger);
 void enviar_status_clase(int* socket, status_clave* status, t_log* logger);
+void enviar_pedido_valor(int* socket, char* clave, t_log* logger);
+void enviar_confirmacion(int* socket, int* confirmacion, int id, t_log* logger);
 int recibir_confirmacion(int* socket, t_log* logger);
+char* recibir_pedido_clave(int* socket, t_log* logger);
+char* recibir_valor(int* socket, t_log* logger);
+t_esi_operacion recibir_instruccion(int* socket, t_log* logger);
 
+//FUNCIONAMIENTO INTERNO
 int handshake(int* socket, t_log* logger);
+void procesar_mensaje(int id);
+void conectar_esi(int* socket, int id_recibido, t_log* logger);
+void conectar_instancia(int* socket, int id_recibido, t_log* logger);
 
 #endif /* COORDINADOR_H_ */
--- Coordinador/coordinador_funciones.h
+++ Coordinador/coordinador_funciones.h
@@ -37,58 +37,78 @@ void conectar_planificador(int* socket_escucha, int* socket_planificador, t_log*
 
 void enviar_configuracion_instancia(int* socket, info_archivo_config configuracion, t_log* logger){
 	datos_configuracion mensaje = {*configuracion.tamano_entrada, *configuracion.cantidad_entradas};
+	//serializar
 	enviar(socket, &mensaje, sizeof(datos_configuracion), 00, logger);
 }
 
 void enviar_pedido_esi(int esi_id, int* socket, t_esi_operacion instruccion, t_log* logger){
 	pedido_esi pedido = {esi_id, instruccion};
+	//serializar
 	enviar(socket, &pedido, sizeof(pedido), 43, logger);
 }
 
 void enviar_status_clase(int* socket, status_clave* status, t_log* logger){
+	//serializar
 	enviar(socket, status, sizeof(status_clave), 44, logger);
 }
 
 void enviar_pedido_valor(int* socket, char* clave, t_log* logger){
+	//serializar
 	enviar(socket, clave, sizeof(clave), 01, logger);
+}
+
+void enviar_confirmacion(int* socket, int* confirmacion, int id, t_log* logger){
+	//serializa
+	enviar(socket, confirmacion, sizeof(int), id, logger);
 }
 
 int recibir_confirmacion(int* socket, t_log* logger){
 	int confirmacion;
 	recibir(socket, &confirmacion, sizeof(int), logger);
+	//deserializar
 	return confirmacion;
 }
 
 char* recibir_pedido_clave(int* socket, t_log* logger){
 	char* clave;
 	recibir(socket, clave, sizeof(clave), logger);
+	//deserializar?
 	return clave;
 }
 
 char* recibir_valor(int* socket, t_log* logger){
 	char* valor;
 	recibir(socket, valor, sizeof(valor), logger);
+	//deserializar?
 	return valor;
+}
+
+t_esi_operacion recibir_instruccion(int* socket, t_log* logger){
+	t_esi_operacion instruccion;
+	recibir(socket, &instruccion, sizeof(t_esi_operacion), logger);
+	//deserializar
+	return instruccion;
 }
 
 
 /////////////////////// FUNCIONAMIENTO INTERNO ///////////////////////
 
-int handshake(int* socket, t_log* logger){
+int handshake(int* socket_cliente, t_log* logger){
 	int conexion_hecha = 0;
 
 	t_handshake proceso_recibido;
 	t_handshake yo = {COORDINADOR, 0};
 	int id_recibido;
 
-	enviar(socket, &yo, sizeof(t_handshake), 80, logger);
-	recibir(socket, &id_recibido, sizeof(int), logger);
+	enviar(socket_cliente, &yo, sizeof(t_handshake), 80, logger);
+	recibir(socket_cliente, &id_recibido, sizeof(int), logger);
 
 	if(id_recibido != 80){
+		log_info(logger, "Conexion invalida");
 		return -1;
 	}
 
-	recibir(socket, &proceso_recibido, sizeof(t_handshake), logger);
+	recibir(socket_cliente, &proceso_recibido, sizeof(t_handshake), logger);
 
 	switch(proceso_recibido.proceso){
 	case PLANIFICADOR:
@@ -100,11 +120,11 @@ int handshake(int* socket, t_log* logger){
 		}
 		break;
 	case INSTANCIA:
-		//manejar_instancia();
+		conectar_instancia(socket_cliente, id_recibido, logger);
 		return 1;
 		break;
 	case ESI:
-		//manejar_esi();
+		conectar_esi(socket_cliente, id_recibido, logger);
 		return 1;
 		break;
 	case COORDINADOR:
@@ -117,18 +137,48 @@ int handshake(int* socket, t_log* logger){
 
 }
 
-/*void procesar_conexiones(int socket){
+void procesar_conexion(int* socket_escucha, t_log* logger){
+	int socket_cliente = aceptar_conexion(socket_escucha, logger);
+	handshake(&socket_cliente, logger);
+}
+
+void conectar_esi(int* socket, int id_recibido, t_log* logger){
+	int id_proceso = id_recibido;
+	int id_mensaje;
 	while(1){
-		//int cliente = aceptar_conexion();
-		int cliente;
-		handshake(cliente);
+		recibir(socket, &id_mensaje, sizeof(int), logger);
+		procesar_mensaje(id_mensaje);
 	}
 }
 
+void conectar_instancia(int* socket, int id_recibido, t_log* logger){
+	int id_proceso = id_recibido;
+	int id_mensaje;
+	while(1){
+		recibir(socket, &id_mensaje, sizeof(int), logger);
+		procesar_mensaje(id_mensaje);
+	}
+}
 
+void procesar_mensaje(int id){
+	switch(id){
+		case 20:
 
+		break;
+		case 21:
 
+		break;
+		case 22:
 
+		break;
+		case 23:
+
+		break;
+		default:
+
+		break;
+	}
+}
 
 /*int atender_conexion_esi(clave){ //recibe la solicitud del esi con la clave que quiere
 	//acceder
