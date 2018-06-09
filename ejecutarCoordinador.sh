600626a4c484a70ed7cc789d89862cab96ea69ea
Your Name <imazzini@est.frba.utn.edu.ar>
2018-06-09 13:06:56 -0300

    cambios en el script del coordinador

--- ejecutarCoordinador.sh
+++ ejecutarCoordinador.sh
@@ -2,13 +2,9 @@
 
 export LD_LIBRARY_PATH=$PWD/Shared_Libraries/commons_propias/Debug
 
-<<<<<<< Updated upstream
-./Coordinador/Debug/Coordinador
-=======
 cd Coordinador/Debug
->>>>>>> Stashed changes
 
 make clean
 make all
 
-./coordinador_copia 
+./Coordinador 
