55587683ef6f167c9cc8d0474ad04baa0a5baff5
lusalmeron <lucila@salmeron.com.ar>
2018-06-25 21:20:27 -0300

    Ejecutores de pruebas ESI

---
+++ ejecutarESI_Prueba2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=./Shared_Libraries/commons_propias/Debug
+
+./ESI/Debug/ESI 7 "ESI/Prubeas-ESI/ESI_1.txt" "ESI/Prubeas-ESI/ESI_1.txt" "ESI/Prubeas-ESI/ESI_1.txt" "ESI/Prubeas-ESI/ESI_Largo.txt" "ESI/Prubeas-ESI/ESI_1.txt" "ESI/Prubeas-ESI/ESI_1.txt"
---
+++ ejecutarESI_Prueba4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=./Shared_Libraries/commons_propias/Debug
+
+./ESI/Debug/ESI 3 "ESI/Prubeas-ESI/ESI_MultiClave.txt" "ESI/Prubeas-ESI/ESI_Largo.txt"
