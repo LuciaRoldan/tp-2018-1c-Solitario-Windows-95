3a19f6b3d0705c07aa38e89a272c43dad704f151
lusalmeron <lucila@salmeron.com.ar>
2018-07-12 02:41:54 -0300

    Una o varias sentencias de ejecucion fallan. En revisión

--- Ejecutables_ESI_Checkpoint/ejecutarESI_Prueba4.sh
+++ Ejecutables_ESI_Checkpoint/ejecutarESI_Prueba4.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=./Shared_Libraries/commons_propias/Debug
+export LD_LIBRARY_PATH=$PWD/../Shared_Libraries/commons_propias/Debug
 
 cd ../ESI/Debug/ 
+
 make clean 
 make all
+
 cd ../../
 
-./ESI/Debug/ESI "ESI/Prubeas-ESI/ESI_MultiClave" 1
+./ESI/Debug/ESI "/home/utnso/workspace/tp-2018-1c-Solitario-Windows-95/ESI/Prubeas-ESI/ESI_MultiClave" 1
 
-./ESI/Debug/ESI "ESI/Prubeas-ESI/ESI_Largo" 2
+./ESI/Debug/ESI "/home/utnso/workspace/tp-2018-1c-Solitario-Windows-95/ESI/Prubeas-ESI/ESI_Largo" 2
