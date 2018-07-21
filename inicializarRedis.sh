6a13969cf3ed06cab2e0de5a854e185d7ab590da
lusalmeron <lucila@salmeron.com.ar>
2018-07-21 10:04:35 -0300

    Creo dhell de inicializacion y borro shells de ejecucion extra

---
+++ inicializarRedis.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ../
+# // - - workspace - - //
+
+# - - -  CLONAR SO-COMMONS - - - 
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library/
+sudo make install
+cd ../
+
+# - - -  CLONAR PARSI - - - 
+git clone https://github.com/sisoputnfrba/parsi.git
+cd parsi/
+sudo make install
+cd ../
+
+# - - - CREAR CARPETAS DE INSTANCIA - - - 
+mkdir Inst1
+mkdir Inst2
+mkdir Inst3
+
+# - - - BUILDEAR COMMONS_PROPIAS - - - 
+cd tp-2018-1c-Solitario-Windows-95/Shared_Libraries/commons_propias/Debug/
+sudo make clean
+sudo make all
+cd ../..
+
+# - - - BUILDEAR COMMONS - - - 
+cd commons/Debug/
+sudo make clean
+sudo make all
+cd ../../..
+
