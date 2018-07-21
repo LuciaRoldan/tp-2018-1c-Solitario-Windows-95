a4c3e400031b504a2f308543373af2714816d4e3
LuciaRoldan <lucia_roldan@outlook.com>
2018-07-21 10:00:14 -0300

    Prueba 2

---
+++ prueba2/instancia1BSU.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$PWD/../Shared_Libraries/commons_propias/Debug
+
+cd ../Instancia/Debug
+
+make clean
+make all
+
+export LD_LIBRARY_PATH=$PWD/../../Shared_Libraries/commons_propias/Debug
+
+./instancia "../../prueba2/Configuracion_instancia1BSU.txt" 1
