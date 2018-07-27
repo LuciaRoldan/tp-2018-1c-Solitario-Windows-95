417b8ecb152e898a762decf05be6d7398c479f1c
lusalmeron <lucila@salmeron.com.ar>
2018-07-26 23:33:46 -0300

    Cambio nombres de ESIs de pruebas

---
+++ prueba2/esi2_Compactador2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../ESI/Debug/
+
+make clean
+make all
+
+cd ../../
+
+export LD_LIBRARY_PATH=$PWD/Shared_Libraries/commons_propias/Debug
+
+./ESI/Debug/ESI "ESI/TestFinales/ESI_Compactador2" 2
+#El documento de Tests nombra tanto el 1 como el 2. Supongo que el que va es el 2.
