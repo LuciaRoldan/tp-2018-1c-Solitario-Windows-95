9faacbb45bcc9e168bf8aff4e900540e7c9299b7
LuciaRoldan <lucia_roldan@outlook.com>
2018-08-04 00:38:12 -0300

    Agrego la prueba 6

---
+++ prueba6/esi5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$PWD/../Shared_Libraries/commons_propias/Debug
+
+cd ../ESI/Debug/
+
+make clean
+make all
+
+cd ../../
+
+export LD_LIBRARY_PATH=$PWD/Shared_Libraries/commons_propias/Debug
+
+./ESI/Debug/ESI "ESI/TestFinales/ESI_MenuParrilla" 5
